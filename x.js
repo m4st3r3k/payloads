f3c2f0b245b650d93d974bdbea94c63a8a659548
m4st3r3k <31544067+m4st3r3k@users.noreply.github.com>
2022-05-20 02:02:04 -0300

    Update x.js

--- x.js
+++ x.js
@@ -1,12 +1,1 @@
-<script>
-function newSite() {
-    var sites = ['http://getprismatic.com',
-                 'http://gizmodo.com/',
-                 'http://lifehacker.com/']
-
-    document.getElementById('myIframe').src = sites[Math.floor(Math.random() * sites.length)];
-}   
-
-newSite();
-
-</script>
+<script> alert(document.cookie) </script>
